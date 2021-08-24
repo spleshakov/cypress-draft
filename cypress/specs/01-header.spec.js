ac986d30d070e1b7a25716c28579bf3d18e9d5e8
sergey <spleshakov@zipari.com>
2021-08-24 18:18:25 -0400

    added test cases for header

--- cypress/specs/01-header.spec.js
+++ cypress/specs/01-header.spec.js
@@ -1,4 +1,5 @@
 import {Header} from "../component-objects/header";
+let header = new Header();
 
 describe('Header test suite', () => {
 
@@ -6,20 +7,56 @@ describe('Header test suite', () => {
 		cy.visit('/');
 	})
 
-	// TODO rename
-	it('Pass!', async () => {
+	it('Navigation: About Us -> Who We Are', async () => {
+		cy.get('.et_pb_slides')
+			.should(
+				'exist'
+			)
 
-		let header = new Header();
-		// Header
-		header.navigateTo('Resource Center', 'Blog')
-		cy.wait(5000)
+		header.navigateTo('About Us', 'Who We Are');
 
-		header.navigateTo('Contact Us')
-		cy.wait(5000)
+		cy.get('.et_pb_slides')
+			.should(
+				'not.exist'
+			)
 
-		header.goHome()
-		cy.wait(5000)
+		cy.get('.header-content h1')
+			.as('pageHeader')
+			.should(
+				'have.text',
+				'Who We Are'
+			)
+	})
 
-		expect(true).to.equal(true)
+	it('Navigation: Contact Us', async () => {
+
+		header.navigateTo('Contact Us');
+
+		cy.get('@pageHeader')
+			.should(
+				'have.text',
+				'Contact Us'
+			)
+	})
+
+	it('Navigation: home page redirection by AristaMD logo', async () => {
+
+		header.goHome();
+
+		cy.get('.et_pb_slides')
+			.should(
+				'exist'
+			)
+	})
+
+	it('Request Demo button', async () => {
+
+		header.navigateTo('Request Demo');
+
+		cy.get('@pageHeader')
+			.should(
+				'have.text',
+				'Request an eConsult Demo'
+			)
 	})
 })
